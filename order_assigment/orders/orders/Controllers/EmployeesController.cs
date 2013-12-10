365166c2ba230e97edeb3ccd102e66b835a0764b
PGuihen <paul.guihen@gmail.com>
2013-12-10 13:56:15 +0000

    employeesadded_notwrking

---
+++ order_assigment/orders/orders/Controllers/EmployeesController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using orders.Models;
+
+namespace orders.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private EmployeesDBContext db = new EmployeesDBContext();
+
+        //
+        // GET: /Employees/
+
+        public ActionResult Index()
+        {
+            return View(db.EmployeesDbSet.ToList());
+        }
+
+        //
+        // GET: /Employees/Details/5
+
+        public ActionResult Details(string id = null)
+        {
+            Employees employees = db.EmployeesDbSet.Find(id);
+            if (employees == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employees);
+        }
+
+        //
+        // GET: /Employees/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Employees/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Employees employees)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EmployeesDbSet.Add(employees);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employees);
+        }
+
+        //
+        // GET: /Employees/Edit/5
+
+        public ActionResult Edit(string id = null)
+        {
+            Employees employees = db.EmployeesDbSet.Find(id);
+            if (employees == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employees);
+        }
+
+        //
+        // POST: /Employees/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Employees employees)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employees).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employees);
+        }
+
+        //
+        // GET: /Employees/Delete/5
+
+        public ActionResult Delete(string id = null)
+        {
+            Employees employees = db.EmployeesDbSet.Find(id);
+            if (employees == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employees);
+        }
+
+        //
+        // POST: /Employees/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Employees employees = db.EmployeesDbSet.Find(id);
+            db.EmployeesDbSet.Remove(employees);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ order_assigment/orders/orders/Models/Employees.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace orders.Models
+{
+     [Table("Employees")]
+    public class Employees
+    {
+        [Key]
+
+        public int EmployeeID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Title { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime HireDate { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Region { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string HomePhone { get; set; }
+        public string Extension { get; set; }
+         //type for photo???
+        public byte Photo { get; set; }
+        public string Notes { get; set; }
+        public int ReportsTo { get; set; }
+        public string PhotoPath { get; set; }
+    }
+     public class EmployeesDBContext : DbContext
+     {
+
+
+         static EmployeesDBContext()
+         {
+             Database.SetInitializer<Models.EmployeesDBContext>(null);
+         }
+
+
+
+         public DbSet<Employees> EmployeesDbSet { get; set; }
+     }
+}
