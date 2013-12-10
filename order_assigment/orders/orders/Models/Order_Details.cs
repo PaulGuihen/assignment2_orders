941f3e60f31c69c2eb8b746fff8cb13765eb00b8
PGuihen <paul.guihen@gmail.com>
2013-12-10 13:58:20 +0000

    orders empllyees done

---
+++ order_assigment/orders/orders/Models/Order_Details.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace orders.Models
+{
+    [Table("Order Details")]
+    public class Order_Details
+    {
+       
+
+    
+         [Key]
+
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public Decimal UnitPrice { get; set; }
+        public Int16 Quantity { get; set; }
+        public Single Discount { get; set; }
+    }
+
+    public class Order_DetailsDBContext : DbContext
+    {
+
+
+        static Order_DetailsDBContext()
+        {
+            Database.SetInitializer<Models.Order_DetailsDBContext>(null);
+        }
+
+
+
+        public DbSet<Order_Details> Order_DetailsDbSet { get; set; }
+    }
+
+}
